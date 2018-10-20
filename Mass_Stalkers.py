a0f37a3075a6f8e84907dd1568b2c7dfc42c7f2a
qpalzmal <mshan0290@gmail.com>
2018-10-19 17:47:55 -0700

    working core/gateway

--- Mass_Stalkers.py
+++ Mass_Stalkers.py
@@ -17,72 +17,71 @@ class MassStalkerBot(sc2.BotAI):
         await self.distribute_workers()
         await self.build_workers()
         await self.build_supply()
-        await self.build_assimilators()
-        await self.build_gateways()
+        await self.build_assimilator()
         await self.build_cybernetics()
+        await self.build_gateways()
         await self.build_stalkers()
 
         if self.units(CYBERNETICSCORE).ready and self.can_afford(RESEARCH_WARPGATE) and not self.warp_researched:
             self.warp_researched = True
-            await self.chrono_cybernetics()
+            # await self.chrono_cybernetics()
             await self.do(self.units(CYBERNETICSCORE).ready.first(RESEARCH_WARPGATE))
 
-        if self.units(CYBERNETCSCORE).ready.exists and self
+        # if self.units(CYBERNETCSCORE).ready.exists and self
 
-
-        if self.supply_cap - self.supply_used <= 10:
-            await self.do(self.units(STALKER))
+        if self.supply_used >= 75:
+            for stalker in self.units(STALKER):
+                await self.do(stalker.attack(self.enemy_start_locations[0]))
 
     # checks all nexus if they are queued up, if not queue up a probe
     async def build_workers(self):
         for nexus in self.units(NEXUS).ready.noqueue:
-            if self.can_afford(PROBE) and self.units(NEXUS) * 22 <= self.units(PROBE):
+            if self.can_afford(PROBE) and self.units(NEXUS).amount * 22 > self.units(PROBE).amount:
                 await self.do(nexus.train(PROBE))
 
     # builds a pylon if there isn't one being made and if there is only 10 or less supply left
     async def build_supply(self):
-        if self.supply_left <= 8 and not self.already_pending(PYLON):
+        if self.supply_left <= 6 and not self.already_pending(PYLON):
             nexus = self.units(NEXUS).ready
             if nexus.exists:
                 if self.can_afford(PYLON):
-                    await self.build(PYLON, near=nexus.first)
+                    await self.build(PYLON, near=nexus.random)
 
     # builds assimilator if there are any gateways
-    async def build_assimilators(self):
+    async def build_assimilator(self):
         for nexus in self.units(NEXUS).ready:
             # finds all the vespene geyser that is by each nexus
             self.vespene_geysers = self.state.vespene_geyser.closer_than(10.0, nexus)
             for vespene_geyser in self.vespene_geysers:
                 # checks if can afford to make assmililator and there is already a gateway warping in
-                if not self.can_afford(ASSIMILATOR)\
-                        and not self.already_pending(GATEWAY) or self.units(GATEWAY).not_ready:
-                    break
-                worker = self.select_build_worker(vespene_geyser.position)
-                if worker is None:
-                    break
-                # checks if there is already a assimilator at the geyser, if not builds an assimilator
-                if not self.units(ASSIMILATOR).closer_than(1.0, vespene_geyser).exists:
-                    await self.do(worker.build(ASSIMILATOR, vespene_geyser))
+                if self.can_afford(ASSIMILATOR) and self.already_pending(GATEWAY) or self.units(GATEWAY).ready.exists:
+                    # checks if there is already a assimilator at the geyser, if not builds an assimilator
+                    worker = self.select_build_worker(vespene_geyser.position)
+                    if not self.units(ASSIMILATOR).closer_than(1.0, vespene_geyser).exists:
+                        await self.do(worker.build(ASSIMILATOR, vespene_geyser))
 
     # builds a gateways of there are 8 or less
     async def build_gateways(self):
-        if self.can_afford(GATEWAY) and self.units(GATEWAY).ready.exists <= 8:
-            pylon = self.units(PYLON).ready.random
-            await self.do(worker.build(GATEWAY, near=pylon))
+        if self.units(PYLON).ready.exists:
+            if self.can_afford(GATEWAY) and self.units(GATEWAY).amount <= 8:
+                await self.build(GATEWAY, near=self.units(PYLON).ready.random, max_distance=6)
 
     # builds a cybernetics if there isn't one already
     async def build_cybernetics(self):
-        if self.units(GATEWAY).ready and self.can_afford(CYBERNETICSCORE) and self.units(CYBERNETICSCORE).not_ready:
-            pylon = self.units(PYLON).ready.random
-            await self.do(worker.build(CYBERNETICSCORE, near=pylon))
+        if not self.units(CYBERNETICSCORE).exists:
+            if self.units(GATEWAY).ready.exists and self.can_afford(CYBERNETICSCORE)\
+                    and not self.already_pending(CYBERNETICSCORE):
+                await self.build(CYBERNETICSCORE, near=self.units(PYLON).ready.random)
 
-    async def chrono_cybernetics(self):
-        for nexus in self.units(NEXUS).ready.exists:
-            if nexus.energy >= 50 and not self.units(CYBERNETICSCORE).
+    # async def chrono_cybernetics(self):
+    #     for nexus in self.units(NEXUS).ready.exists:
+    #         if nexus.energy >= 50 and not self.units(CYBERNETICSCORE).
 
     async def build_stalkers(self):
-        for gateway in self.units(GATEWAY).ready.noqueue and self.can_afford(STALKER):
-            await self.do(gateway.train(STALKER))
+        if self.units(CYBERNETICSCORE).ready.exists:
+            for gateway in self.units(GATEWAY).ready.noqueue:
+                if self.can_afford(STALKER):
+                    await self.do(gateway.train(STALKER))
 
 
 def main():
@@ -90,7 +89,7 @@ def main():
         # Human(Race.Protoss),
         Bot(Race.Protoss, MassStalkerBot()),
         Computer(Race.Protoss, Difficulty.VeryEasy)
-    ], realtime=True)
+    ], realtime=False)
 
 
 if __name__ == "__main__":
