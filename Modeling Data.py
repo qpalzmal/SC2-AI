4cd2b2ff5b88a476c6367d6824a36630fec193d1
Morgan Shan <mshan0290@gmail.com>
2018-11-09 10:39:05 -0700

    working

--- Modeling Data.py
+++ Modeling Data.py
@@ -22,3 +22,102 @@ model.add(Conv2D(128, (3, 3), padding="same", activation="relu"))
 model.add(Conv2D(128, (3, 3), activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
+
+model.add(Flatten())
+model.add(Dense(512, activation="relu"))
+model.add(Dropout(0.5))
+
+model.add(Dense(4, activation="softmax"))
+
+learning_rate = 0.0001
+opt = keras.optimizers.adam(lr=learning_rate, decay=1e-4)
+
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+# saves logging data
+tensorboard = TensorBoard(log_dir="logs/stage1")
+
+training_data_dir = "Training Data"
+
+
+# gets lengths of all lists and returns it
+def check_data():
+    choices = {"no_attacks": no_attacks,
+               "attack_closest_to_nexus": attack_closest_to_nexus,
+               "attack_enemy_structures": attack_enemy_structures,
+               "attack_enemy_start": attack_enemy_start}
+
+    total_data = 0
+
+    lengths = []
+    for choice in choices:
+        print("Length of {} is: {}".format(choice, len(choices[choice])))
+        total_data += len(choices[choice])
+        lengths.append(len(choices[choice]))
+
+    print("Total data length now is: ", total_data)
+    return lengths
+
+
+hm_epochs = 10
+
+for i in range(hm_epochs):
+    current = 0
+    increment = 200
+    not_maximum = True
+    all_files = os.listdir(training_data_dir)
+    maximum = len(all_files)
+    random.shuffle(all_files)
+
+    while not_maximum:
+        print("Currently doing {}:{}".format(current, current + increment))
+        no_attacks = []
+        attack_closest_to_nexus = []
+        attack_enemy_structures = []
+        attack_enemy_start = []
+        for file in all_files[current:current + increment]:
+            full_path = os.path.join(training_data_dir, file)
+            data = np.load(full_path)
+            data = list(data)
+            for d in data:
+                choice = np.argmax(d[0])
+                if choice == 0:
+                    no_attacks.append(d)
+                elif choice == 1:
+                    attack_closest_to_nexus.append(d)
+                elif choice == 2:
+                    attack_enemy_structures.append(d)
+                elif choice == 3:
+                    attack_enemy_start.append(d)
+
+        lengths = check_data()
+        lowest_data = min(lengths)
+
+        data_type = [no_attacks, attack_closest_to_nexus, attack_enemy_structures, attack_enemy_start]
+
+        for i in range(len(data_type)):
+            random.shuffle(data_type[i])
+            data_type[i] = data_type[i][:lowest_data]
+
+        check_data()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
