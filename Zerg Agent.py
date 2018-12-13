a1e19850d472a68276aac2c6e262eead2196a054
Morgan Shan <mshan0290@gmail.com>
2018-12-13 10:39:20 -0700

    agent plays now

--- Zerg Agent.py
+++ Zerg Agent.py
@@ -6,6 +6,12 @@ import random
 
 
 class ZergAgent(base_agent.BaseAgent):
+    def __init__(self):
+        super(ZergAgent, self).__init__()
+
+        self.attack_coordinates = None
+
+    # checks if the current unit type is selected
     def unit_type_is_selected(self, obs, unit_type):
         # if something is selected and checks to see if the first selecting thing is same as passed unit type
         if len(obs.observation.single_select) > 0 and obs.observation.single_select[0].unit_type == unit_type or \
@@ -14,6 +20,7 @@ class ZergAgent(base_agent.BaseAgent):
 
         return False
 
+    # returns a list of all units of that type
     def get_units_by_type(self, obs, unit_type):
         units = []
         for unit in obs.observation.feature_units:
@@ -21,20 +28,63 @@ class ZergAgent(base_agent.BaseAgent):
                 units.append(unit)
         return units
 
+    # checks if the agent meets all criteria to do the given action
+    def can_do(self, obs, action):
+        return action in obs.observation.available_actions
+
     # step() is similar to on_step() from sc2 library
     def step(self, obs):
         super(ZergAgent, self).step(obs)
 
-        # checks if agent meets all criteria to build a spawning pool and only builds 1 spawning pool
+        # obs.first() checks if its first step
+        if obs.first():
+            player_y, player_x = (obs.observation.feature_minimap.player_relative == features.PlayerRelative.SELF).nonzero()
+
+            x_mean = player_x.mean()
+            y_mean = player_y.mean()
+
+            if x_mean <= 31 and y_mean <= 31:
+                self.attack_coordinates = (49, 49)
+            else:
+                self.attack_coordinates = (12, 16)
+
+        # creates overlord if supply <= 4 and all criteria met
+        supply_left = obs.observation.player.food_cap - obs.observation.player.food_used
+        if supply_left <= 4 and self.can_do(obs, actions.FUNCTIONS.Train_Overlord_quick.id):
+                return actions.FUNCTIONS.Train_Overlord_quick("now")
+
+        # creates 1 spawning pool if all criteria met
         spawning_pools = self.get_units_by_type(obs, units.Zerg.SpawningPool)
         if len(spawning_pools) == 0:
-            if self.unit_type_is_selected(obs, units.Zerg.Drone):
-                if actions.FUNCTIONS.Build_SpawningPool_screen.id in obs.observation.available_actions:
+            if self.unit_type_is_selected(obs, units.Zerg.Drone) \
+             and self.can_do(obs, actions.FUNCTIONS.Build_SpawningPool_screen.id):
                     x = random.randint(0, 83)
                     y = random.randint(0, 83)
                     return actions.FUNCTIONS.Build_SpawningPool_screen("now", (x, y))
 
-        # gets all units then adds them to the drones list if that unit is a drone
+        # creates zerglings if all criteria met
+        if self.unit_type_is_selected(obs, units.Zerg.Larva) \
+                and self.can_do(obs, actions.FUNCTIONS.Train_Zergling_quick.id):
+            return actions.FUNCTIONS.Train_Zergling_quick("now")
+
+        # attack with zerglings
+        zerglings = self.get_units_by_type(obs, units.Zerg.Zergling)
+        if len(zerglings) > 20:
+            # attacks with all zerglings if they are selected
+            if self.unit_type_is_selected(obs, units.Zerg.Zergling) \
+             and self.can_do(obs, actions.FUNCTIONS.Attack_minimap.id):
+                return actions.FUNCTIONS.Attack_minimap("now", self.attack_coordinates)
+            # select the zerglings since they aren't selected
+            if self.can_do(obs, actions.FUNCTIONS.select_army.id):
+                return actions.FUNCTIONS.select_army("select")
+
+        # selects random larva
+        larvae = self.get_units_by_type(obs, units.Zerg.Larva)
+        if len(larvae) > 0:
+            larva = random.choice(larvae)
+            return actions.FUNCTIONS.select_point("select_all_type", (larva.x, larva.y))
+
+        # selects random drone
         drones = self.get_units_by_type(obs, units.Zerg.Drone)
         if len(drones) > 0:
             drone = random.choice(drones)
@@ -44,7 +94,8 @@ class ZergAgent(base_agent.BaseAgent):
         return actions.FUNCTIONS.no_op()
 
 
-def main():
+# main() requires a argument so put a placeholder that won't be used
+def main(unused_argv):
     agent = ZergAgent()
     try:
         while True:
